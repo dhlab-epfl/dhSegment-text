e1d645ba57082ad3a3973f46c8654751c6f36b61
Raphael Barman <raphael.barman@epfl.ch>
2019-05-28 21:10:06 +0200

    Fixed pca encoder

--- dh_segment_text/embeddings/pca_encoder.py
+++ dh_segment_text/embeddings/pca_encoder.py
@@ -4,12 +4,12 @@ import tensorflow as tf
 import numpy as np
 
 class PCAEncoder(EmbeddingsEncoder):
-    def __init__(self, pca_mean_path: str, pca_components_path: str, target_dim: int, is_training: bool=False):
+    def __init__(self, pca_mean_path: str, pca_components_path: str, target_dim: int):
         self.pca_mean = tf.constant(np.load(pca_mean_path), dtype=tf.float32)
         self.pca_components = tf.constant(np.load(pca_components_path), dtype=tf.float32)
         self.target_dim = target_dim
 
-    def __call__(self, embeddings: tf.Tensor, embeddings_map: tf.Tensor, target_shape: tf.Tensor) -> tf.Tensor:
+    def __call__(self, embeddings: tf.Tensor, embeddings_map: tf.Tensor, target_shape: tf.Tensor, is_training: bool=False) -> tf.Tensor:
         with tf.variable_scope("PCAEncoder"):
             reduced_components = tf.transpose(self.pca_components[:self.target_dim])
             reduced_embeddings = tf.einsum('aij,jk->aik', (embeddings-self.pca_mean), reduced_components)
